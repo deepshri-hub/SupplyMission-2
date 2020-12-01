1a1a7421a5c29775faea8895726103f03764f900
deepshri-hub <71260446+deepshri-hub@users.noreply.github.com>
2020-12-01 14:42:37 +0530

    Add files via upload

---
+++ RedBox.js
@@ -0,0 +1,10 @@
+class RedBox{
+    constructor(){
+        var options = {
+            'restitution':0.4,
+            'stiffness': 0.04,
+            'length': 10,
+            }
+        }
+        }
+        
